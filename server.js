fb53390a7956d10e2cda88babbb5483ce328b8d6
Ola Faxe <olafaxe@gmail.com>
2019-09-29 21:16:34 +0200

    forcesecure test

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 5000;
 
-server.use(
-  forceSecure(["example.com", "http://news-of-yesterday.herokuapp.com/"])
-);
+server.use(forceSecure(["example.com", "news-of-yesterday.herokuapp.com"]));
 
 server.use(middlewares);
 server.use(router);
